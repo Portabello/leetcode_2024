8bfacc136edd4752bbfa6c625cd293185cdbbb9c
Jasmit Kakkar <jasmitkakkar369@gmail.com>
2023-12-25 22:02:52 -0800

    Update 141.linked_list_cycle.py

--- 141.linked_list_cycle.py
+++ 141.linked_list_cycle.py
@@ -36,7 +36,7 @@ Constraints:
 
 The number of the nodes in the list is in the range [0, 104].
 -105 <= Node.  val <= 105
-pos is -1 or a valid  index in the linked-list.
+pos is -1 or a valid   index in the linked-list.
 """
 # Definition for singly-linked list.
 # class ListNode:
