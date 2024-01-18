3233dc2ed8d14f23930aa03da994a5922cfb8000
Jasmit Kakkar <jasmitkakkar369@gmail.com>
2024-01-18 00:58:13 -0800

    new files

--- 70.climbing_stairs.py
+++ 70.climbing_stairs.py
@@ -1,19 +1,55 @@
-answer = 0
+"""
+70. Climbing Stairs
+Solved
+Easy
+Topics
+Companies
+Hint
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+"""
 class Solution:
     def climbStairs(self, n: int) -> int:
-        global answer
-        answer=0
-        self.step(n)
-        return answer
-    def step(self, n: int) -> int:
-        global answer
-        #end con
-        if(n==0):
-            answer+=1
-            return
-        #1 step
-        if(n-1>=0):
-            self.step(n-1)
-        #2 step
-        if(n-1>=0):
-            self.step(n-2)
+        one,two = 1,1
+        for x in range(n-1):
+            tmp = one
+            one = one + two
+            two = tmp
+        return one
+        
+        
+    """    o(n^2)
+        ans = self.r_f(0,n)
+        return ans
+    def r_f(self, x, n):
+        if x>n:
+            return 0
+        if x==n:
+            return 1
+        if x<n:
+            return self.r_f(x+1, n) + self.r_f(x+2, n)
+    """
