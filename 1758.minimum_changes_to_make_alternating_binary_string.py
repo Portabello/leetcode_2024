2c8ebd98aafe22a7dd7e994c45a53637aedf3715
Jasmit Kakkar <jasmitkakkar369@gmail.com>
2024-03-15 19:58:29 -0700

    Create 1758.minimum_changes_to_make_alternating_binary_string.py

---
+++ 1758.minimum_changes_to_make_alternating_binary_string.py
@@ -0,0 +1,62 @@
+"""
+1758. Minimum Changes To Make Alternating Binary String
+Solved
+Easy
+Topics
+Companies
+Hint
+You are given a string s consisting only of the characters '0' and '1'. In one operation, you can change any '0' to '1' or vice versa.
+
+The string is called alternating if no two adjacent characters are equal. For example, the string "010" is alternating, while the string "0100" is not.
+
+Return the minimum number of operations needed to make s alternating.
+
+ 
+
+Example 1:
+
+Input: s = "0100"
+Output: 1
+Explanation: If you change the last character to '1', s will be "0101", which is alternating.
+Example 2:
+
+Input: s = "10"
+Output: 0
+Explanation: s is already alternating.
+Example 3:
+
+Input: s = "1111"
+Output: 2
+Explanation: You need two operations to reach "0101" or "1010".
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s[i] is either '0' or '1'.
+"""
+class Solution:
+    def minOperations(self, s: str) -> int:
+        #startzero
+        count_zero = 0
+        flip = '0'
+        for x in s:
+            if x != flip:
+                count_zero += 1
+            if flip == '0':
+                flip = '1'
+            else:
+                flip = '0'
+        print(count_zero)
+        #startone
+        count_one = 0
+        flip = '1'
+        for x in s:
+            if x != flip:
+                count_one += 1
+            if flip == '0':
+                flip = '1'
+            else:
+                flip = '0'
+
+        return min(count_zero, count_one)
