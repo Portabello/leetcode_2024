2766ddee45f83d81644231efe03dc78803b629bb
Jasmit Kakkar <jasmitkakkar369@gmail.com>
2024-05-11 13:21:30 -0700

    Update 8.string_to_integer_atoi.py

--- 8.string_to_integer_atoi.py
+++ 8.string_to_integer_atoi.py
@@ -44,7 +44,7 @@ Step 2: "   -42" ('-' is read, so the result should be negative)
 Step 3: "   -42" ("42" is read in)
                ^
 The parsed integer is -42.
-Since -42 is in the range [-231, 231 - 1], the final result is -42.
+Since -42  is in the range [-231, 231 - 1], the final result is -42.
 Example 3:
 
 Input: s = "4193 with words"
