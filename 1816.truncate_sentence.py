e9258387dda1d1719a550fc454c1fbdba9f8b241
Jasmit Kakkar <jasmitkakkar369@gmail.com>
2024-05-11 16:13:32 -0700

    Update 1816.truncate_sentence.py

--- 1816.truncate_sentence.py
+++ 1816.truncate_sentence.py
@@ -10,7 +10,7 @@ A sentence is a list of words that are separated by a single space with no leadi
 For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
 You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
 
- 
+
 
 Example 1:
 
@@ -25,14 +25,14 @@ Example 2:
 Input: s = "What is the solution to this problem", k = 4
 Output: "What is the solution"
 Explanation:
-The words in s are ["What" , "is" "the", "solution", "to", "this", "problem"].
+The words in s are ["What" , "is" "the", "s olution", "to", "this", "problem"].
 The first 4 word s are ["What", "is", "the", "solution"].
 Hence, you should return "What is the solution".
 Example 3:
 
 Input: s =  "chopper is not  a tanuki", k = 5
 Output: "chopper is not a  tanuki"
- 
+
 
 Constraints:
 
