2b0bbcb4885213ac628b400dc685bab57c365ac6
Jasmit Kakkar <jasmitkakkar369@gmail.com>
2024-06-25 09:52:17 -0700

    Create 1.two_sum.py

---
+++ blind 75/array/1.two_sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i,x in enumerate(nums):
+            if target-x in nums and nums.index(target-x)!=i:
+                return [i,nums.index(target-x)]
+#hash solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i,x in enumerate(nums):
+            if target-x in hashmap:
+                return [i,hashmap[target-x]]
+            hashmap[x] = i
