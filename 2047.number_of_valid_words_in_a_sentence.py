e994fe680be41a639787efd56c2554de244ebd55
Jasmit Kakkar <jasmitkakkar369@gmail.com>
2024-08-25 00:02:13 -0700

    Update 2047.number_of_valid_words_in_a_sentence.py

--- 2047.number_of_valid_words_in_a_sentence.py
+++ 2047.number_of_valid_words_in_a_sentence.py
@@ -8,7 +8,7 @@ Hint
 
 A sentence consists of lowercase letters ('a' to 'z'), digits ('0' to '9'), hyphens ('-'), punctuation marks ('!', '.', and ','), and spaces (' ') only. Each sentence can be broken down into one or more tokens separated by one or more spaces ' '.
 
-A token is a valid word if all three of the following are true:
+A token is a valid word if all  three of the following are true:
 
     It only contains lowercase letters, hyphens, and/or punctuation (no digits).
     There is at most one hyphen '-'. If present, it must be surrounded by lowercase characters ("a-b" is valid, but "-ab" and "ab-" are not valid).
