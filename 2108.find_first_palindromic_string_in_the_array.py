03b42089e9bfd571d7d8b12fabfd1de7c94ad94f
Jasmit Kakkar <jasmitkakkar369@gmail.com>
2024-08-31 21:02:31 -0700

    add

--- 2108.find_first_palindromic_string_in_the_array.py
+++ 2108.find_first_palindromic_string_in_the_array.py
@@ -17,7 +17,7 @@ Example 1:
 Input: words = ["abc","car","ada","racecar","cool"]
 Output: "ada"
 Explanation: The first string that is palindromic is "ada".
-Note that "racecar" is also palindromic, but it is not the first.
+Note that "racecar" is als o palindromic, but it is not the first.
 
 Example 2:
 
