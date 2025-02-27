4efc32d5d24ccc4afed6813b0f1b357c4c009997
Portabello <jasmitkakkar369@gmail.com>
2025-02-27 14:50:47 -0800

    Create 3226.number_of_bit_changes_to_make_two_integers_equal.py

---
+++ 3226.number_of_bit_changes_to_make_two_integers_equal.py
@@ -0,0 +1,66 @@
+'''
+3226. Number of Bit Changes to Make Two Integers Equal
+Solved
+Easy
+Topics
+Companies
+Hint
+
+You are given two positive integers n and k.
+
+You can choose any bit in the binary representation of n that is equal to 1 and change it to 0.
+
+Return the number of changes needed to make n equal to k. If it is impossible, return -1.
+
+
+
+Example 1:
+
+Input: n = 13, k = 4
+
+Output: 2
+
+Explanation:
+Initially, the binary representations of n and k are n = (1101)2 and k = (0100)2.
+We can change the first and fourth bits of n. The resulting integer is n = (0100)2 = k.
+
+Example 2:
+
+Input: n = 21, k = 21
+
+Output: 0
+
+Explanation:
+n and k are already equal, so no changes are needed.
+
+Example 3:
+
+Input: n = 14, k = 13
+
+Output: -1
+
+Explanation:
+It is not possible to make n equal to k.
+
+
+
+Constraints:
+
+    1 <= n, k <= 106
+
+'''
+class Solution:
+    def minChanges(self, n: int, k: int) -> int:
+        n = bin(n)[2:]
+        k = bin(k)[2:]
+        maxlen = max(len(n), len(k))
+        n = (maxlen-len(n))*'0' + n
+        k = (maxlen-len(k))*'0' + k
+        ans = 0
+        for i in range(len(n)):
+            #print(n[i], k[i])
+            if n[i] == '0' and k[i] == '1':
+                return -1
+            elif n[i] != k[i]:
+                ans += 1
+        return ans
