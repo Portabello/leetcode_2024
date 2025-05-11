76af625829986b67ab7ada8821a5bacb26642717
Portabello <jasmitkakkar369@gmail.com>
2025-05-11 11:41:55 -0700

    Update 112.path_sum.py

--- 112.path_sum.py
+++ 112.path_sum.py
@@ -40,7 +40,6 @@ The number of nodes in the tree is in the range [0, 5000].
 """
 
 
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
