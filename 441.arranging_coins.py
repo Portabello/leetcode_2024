8280e97260b66ac2e30f542b2ab1957e54695700
Portabello <jasmitkakkar369@gmail.com>
2025-06-11 12:55:37 -0700

    Update 441.arranging_coins.py

--- 441.arranging_coins.py
+++ 441.arranging_coins.py
@@ -8,7 +8,7 @@ You have n coins and you want to build a staircase with these coins. The stairca
 
 Given the integer n, return the number of complete rows of the staircase you will build.
 
- 
+
 
 Example 1:
 
@@ -23,7 +23,32 @@ Input: n = 8
 Output: 3
 Explanation: Because the 4th row is incomplete, we return 3.
 """
-import math
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        return int((-1 + sqrt(1+8*n))/2)
+        if n==1 or n==2:
+            return 1
+       # if n==3:
+            #return 2
+        def triangular_number(x):
+            return (x*(x+1))/2
+        l,r= 0,n
+        while l<=r:
+            m=(l+r)//2
+            tn = triangular_number(m)
+            #print('iteration l=',l,' m=',m,' r=',r)
+            #print('testing ', tn)
+            if tn>=n:
+                #print('1')
+                #check layer below
+                if tn == n:
+                    return m
+                tnn = triangular_number(m-1)
+                if tnn < n:
+                    return m-1
+            if tn>=n:
+                #print('2')
+                r=m
+            else:
+                #print('3')
+                l=m
+        return -1
