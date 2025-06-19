bfa213d7d25eedfd1a163278ed69717dbfe2110a
Portabello <jasmitkakkar369@gmail.com>
2025-06-19 16:30:03 -0700

    Update 326.power_of_three.py

--- 326.power_of_three.py
+++ 326.power_of_three.py
@@ -8,7 +8,7 @@ Given an integer n, return true if it is a power of three. Otherwise, return fal
 
 An integer n is a power of three, if there exists an integer x such that n == 3x.
 
- 
+
 
 Example 1:
 
@@ -25,25 +25,20 @@ Example 3:
 Input: n = -1
 Output: false
 Explanation: There is no x where 3x = (-1).
- 
+
 
 Constraints:
 
 -231 <= n <= 231 - 1
- 
+
 
 Follow up: Could you solve it without loops/recursion?
 """
-import math
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n <= 0:
-            return False
-        else:
-            count = 0
-            while True:
-                if(pow(3,count) == n):
-                    return True
-                if(pow(3,count) > n):
-                    return False
-                count += 1
+        for i in range(n):
+            if 3**i == n:
+                return True
+            if 3**i >n:
+                return False
+        return False
