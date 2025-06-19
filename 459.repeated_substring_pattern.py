3a95ce128d648e8e96d3881063ccb8fdc703b548
Portabello <jasmitkakkar369@gmail.com>
2025-06-18 18:10:13 -0700

    Update 459.repeated_substring_pattern.py

--- 459.repeated_substring_pattern.py
+++ 459.repeated_substring_pattern.py
@@ -6,7 +6,7 @@ Topics
 Companies
 Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
 
- 
+
 
 Example 1:
 
@@ -22,7 +22,7 @@ Example 3:
 Input: s = "abcabcabcabc"
 Output: true
 Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
- 
+
 
 Constraints:
 
@@ -31,15 +31,9 @@ s consists of lowercase English letters.
 """
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        s_len = len(s)
-        sub = ""
-        sub_l = 0
-        for c in s:
-            sub = sub+c
-            sub_l += 1
-            if sub_l*2 > s_len:
-                return False
-            multiply_length = int(s_len/sub_l)
-            if sub*multiply_length == s:
-                return True
+        for i in range((len(s)//2)):
+            substring = s[0:(i+1)]
+            if len(s)%len(substring)==0:
+                if ((len(s)//len(substring)) * substring) == s:
+                    return True
         return False
