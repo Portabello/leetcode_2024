e9cecb3616ee996b5f2e51c6555d64f5aa6440e1
Portabello <jasmitkakkar369@gmail.com>
2025-08-16 21:02:52 -0700

    Update 7.reverse_integer.py

--- 7.reverse_integer.py
+++ 7.reverse_integer.py
@@ -8,7 +8,7 @@ Given a signed 32-bit integer x, return x with its digits reversed. If reversing
 
 Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
- 
+
 
 Example 1:
 
@@ -22,7 +22,7 @@ Example 3:
 
 Input: x = 120
 Output: 21
- 
+
 
 Constraints:
 
@@ -30,32 +30,13 @@ Constraints:
 
 
 """
-import sys
-sys.set_int_max_str_digits(2147483647)
-answer = ""
-#change to recurse(x/10, 10, ans) and keep mod at 10 instead of incrementing mod and not changing x
 class Solution:
-    def recurse(self, x:int, ans: str):
-        global answer
-        if(x%10 == x):
-            ans += str(x%10)[0]
-            print(x%10," ",str(x%10)[0])
-            answer = ans
-        else:
-            ans += str(x%10)[0]
-            print(x%10," ",str(x%10)[0])
-            x = int(x/10)
-            self.recurse(x, ans)
     def reverse(self, x: int) -> int:
-        if(x <= -2147483648 or x >= 2147483647):
+        negative = 1 if x<0 else 0
+        x_reverse = str(abs(x))[::-1]
+        if int(x_reverse) > (2**31)-1 or int(x_reverse) < (-2**31):
             return 0
-        if(x>0):
-            self.recurse(x, "")
-            if(int(answer) >= 2147483647 or int(answer) <= -2147483648):
-                return 0
-            return int(answer)
-        else:
-            self.recurse(abs(x), "")
-            if(int(answer) >= 2147483647 or int(answer) <= -2147483648):
-                return 0
+        if negative:
+            return int(x_reverse)*-1
+        return int(x_reverse)
             return int(answer)* -1
