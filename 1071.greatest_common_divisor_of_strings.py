d174e1b5bbde0d2a67683d4c6d7cc2b2f671f4e0
Portabello <jasmitkakkar369@gmail.com>
2025-09-05 12:54:57 -0700

    Update 1071.greatest_common_divisor_of_strings.py

--- 1071.greatest_common_divisor_of_strings.py
+++ 1071.greatest_common_divisor_of_strings.py
@@ -9,7 +9,7 @@ For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.
 
 Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
 
- 
+
 
 Example 1:
 
@@ -23,7 +23,7 @@ Example 3:
 
 Input: str1 = "LEET", str2 = "CODE"
 Output: ""
- 
+
 
 Constraints:
 
@@ -32,12 +32,20 @@ str1 and str2 consist of English uppercase letters.
 """
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        len1 = len(str1)
-        len2 = len(str2)
-        max_string = ""
-        ans = ""
-        for s in str1:
-            if((max_string+s)*int(len1/len(max_string+s))==str1 and (max_string+s)*int(len2/len(max_string+s))==str2):
-                ans = max_string + s
-            max_string = max_string + s
-        return ans
+        hs1, hs2 = Counter(str1), Counter(str2)
+        #for x in hs1:
+        #    if max(hs1[x], hs2[x]) % min(hs1[x], hs2[x]) != 0:
+        #        return False
+        substring = str1[0]
+        substring_len = 1
+        valid_solution = ""
+        while len(substring) <= min(len(str1), len(str2)):
+            #print(substring)
+            if substring * (len(str1)//len(substring)) == str1 and substring * (len(str2)//len(substring)) == str2:
+                #print('valid solution found ', substring)
+                valid_solution = substring
+            if len(substring) == min(len(str1), len(str2)):
+                break
+            substring_len += 1
+            substring = str1[0:substring_len]
+        return valid_solution
