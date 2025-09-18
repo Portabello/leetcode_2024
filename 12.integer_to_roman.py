5b014ccbfdace84042721b16eb67ac1bbd0b67c5
Portabello <jasmitkakkar369@gmail.com>
2025-09-18 01:44:45 -0700

    Update 12.integer_to_roman.py

--- 12.integer_to_roman.py
+++ 12.integer_to_roman.py
@@ -18,12 +18,12 @@ For example, 2 is written as II in Roman numeral, just two one's added together.
 
 Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
-I can be placed before V (5) and X (10) to make 4 and 9. 
-X can be placed before L (50) and C (100) to make 40 and 90. 
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given an integer, convert it to a roman numeral.
 
- 
+
 
 Example 1:
 
@@ -40,7 +40,7 @@ Example 3:
 Input: num = 1994
 Output: "MCMXCIV"
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
- 
+
 
 Constraints:
 
@@ -48,46 +48,12 @@ Constraints:
 """
 class Solution:
     def intToRoman(self, num: int) -> str:
+        mapping = [(1000,'M'), (900,'CM'), (500,'D'), (400,'CD'), (100,'C'), (90,'XC'), (50,'L'), (40,'XL'), (10,'X'), (9,'IX'), (5,'V'), (4,'IV'), (1,'I')]
         ans = ""
-        while(num>0):
-            if(num>=1000):
-                ans+="M"
-                num = num-1000
-            elif(num>=900):
-                ans+="CM"
-                num = num-900
-            elif(num>=500):
-                ans+="D"
-                num = num-500
-            elif(num>=400):
-                ans+="CD"
-                num = num-400
-            elif(num>=100):
-                ans+="C"
-                num = num-100
-            elif(num>=90):
-                ans+="XC"
-                num = num-90
-            elif(num>=50):
-                ans+="L"
-                num = num-50
-            elif(num>=40):
-                ans+="XL"
-                num = num-40
-            elif(num>=10):
-                ans+="X"
-                num = num-10
-            elif(num>=9):
-                ans+="IX"
-                num = num-9
-            elif(num>=5):
-                ans+="V"
-                num = num-5
-            elif(num>=4):
-                ans+="IV"
-                num = num-4
-            else:
-                ans+="I"
-                num = num-1
+        while num>0:
+            for x in mapping:
+                if num>=x[0]:
+                    ans = ans + x[1]
+                    num-=x[0]
+                    break
         return ans
-
