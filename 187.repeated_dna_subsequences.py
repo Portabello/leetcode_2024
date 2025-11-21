023f9157f4d3fc47ddc8a13ed2fa6b6f730894e2
Portabello <jasmitkakkar369@gmail.com>
2025-11-21 03:37:39 -0800

    Update 187.repeated_dna_subsequences.py

--- 187.repeated_dna_subsequences.py
+++ 187.repeated_dna_subsequences.py
@@ -11,7 +11,7 @@ When studying DNA, it is useful to identify repeated sequences within the DNA.
 
 Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
 
- 
+
 
 Example 1:
 
@@ -21,7 +21,7 @@ Example 2:
 
 Input: s = "AAAAAAAAAAAAA"
 Output: ["AAAAAAAAAA"]
- 
+
 
 Constraints:
 
@@ -30,16 +30,15 @@ s[i] is either 'A', 'C', 'G', or 'T'.
 """
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        occ = set()
-        ans = []
-        #sliding window of 10
+        hs = {}
         for i in range(len(s)-9):
             #print(s[i:i+10])
-            t = s[i:i+10]
-            if t in occ and t not in ans:
-                ans.append(t)
-            if t not in occ:
-                occ.add(t)
-        #print(occ)
-        #print(ans)
+            if s[i:i+10] in hs:
+                hs[s[i:i+10]]+=1
+            else:
+                hs[s[i:i+10]]=1
+        ans = []
+        for x in hs:
+            if hs[x]>1:
+                ans.append(x)
         return ans
