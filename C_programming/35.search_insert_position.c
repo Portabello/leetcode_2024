b05cd6f5e78d60b02db6756fa83bfb93e826c3a6
Portabello <jasmitkakkar369@gmail.com>
2026-01-12 18:23:50 -0800

    Create 35.search_insert_position.c

---
+++ C_programming/35.search_insert_position.c
@@ -0,0 +1,56 @@
+/*
+35. Search Insert Position
+Solved
+Easy
+Topics
+premium lock iconCompanies
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+
+
+Constraints:
+
+    1 <= nums.length <= 104
+    -104 <= nums[i] <= 104
+    nums contains distinct values sorted in ascending order.
+    -104 <= target <= 104
+
+*/
+int searchInsert(int* nums, int numsSize, int target) {
+    int low = 0;
+    int high = numsSize-1;
+    int mid;
+    while (low <= high){
+        if (low == high){
+            if (nums[low] < target) return low + 1;
+            else return low;
+        }
+        mid = (high + low) / 2;
+        //printf("l:%d  -  r:%d.  - checking %d at index %d\n",low, high, nums[mid],mid);
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return mid;
+
+}
